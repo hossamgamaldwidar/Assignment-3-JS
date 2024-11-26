4064461ae97e46c5d4b130b7216951c5f3739882
Hossam Gamal <hossamgamal2000@outlook.com>
2024-11-26 16:50:05 +0200

    Assignment-3-JS

---
+++ JS/index.js
@@ -0,0 +1,166 @@
+var titleInput = document.getElementById('titleInput');
+var urlInput = document.getElementById('urlInput');
+var submitBtn = document.getElementById('submitBtn');
+var bigWindowAlert1 = document.getElementById('bigWindowAlert1');
+var bigWindowAlert2 = document.getElementById('bigWindowAlert2');
+var bigWindowAlert3 = document.getElementById('bigWindowAlert3');
+var titleInputUpdate = document.getElementById("titleInputUpdate");
+var urlInputUpdate = document.getElementById('urlInputUpdate');
+var lastIndex;
+var bookmarks = [];
+
+if (localStorage.getItem('allBookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('allBookmarks'));
+    displayBookmarks(bookmarks);  // عرض جميع الـ bookmarks عند تحميل الصفحة
+}
+
+function isValidURL(url) {
+    return url.startsWith('http://') || url.startsWith('https://');
+}
+
+function addBookmark() {
+    if (titleInput.value.trim() === '' || urlInput.value.trim() === '') {
+        getAlert();
+        return;
+    }
+    if (titleInput.value.trim().length < 3) {
+        getAlert();
+        return;
+    }
+    if (!isValidURL(urlInput.value.trim())) {
+        getAlert();
+        return;
+    }
+
+    for (var i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].title === titleInput.value) {
+            getAlert2();
+            return;
+        }
+        if (bookmarks[i].url === urlInput.value) {
+            getAlert3();
+            return;
+        }
+    }
+
+    var bookmark = {
+        title: titleInput.value,
+        url: urlInput.value
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('allBookmarks', JSON.stringify(bookmarks));
+    clearBookmarkForm();
+    displayBookmarks(bookmarks); // عرض الـ bookmarks بعد الإضافة
+}
+
+function displayBookmarks(bookmarksToDisplay) {
+    var bookmarksContainer = "";
+    for (var i = 0; i < bookmarksToDisplay.length; i++) {
+        bookmarksContainer += `
+        <div>${i + 1}</div>
+        <div>${bookmarksToDisplay[i].title}</div>
+        <div><a href="${bookmarksToDisplay[i].url}" target="_blank"><button class="visit-btn"><i class="fa-solid fa-eye"></i> Visit</button></a></div>
+        <div><button class="delete-btn" onclick="deleteBookmark(${i})"><i class="fa-solid fa-trash-can"></i> Delete</button></div>
+        <div><button class="update-btn" onclick="getUpdatAlert(${i})"><i class="fa-solid fa-pen-to-square"></i> Update</button></div>
+        `;
+    }
+    document.getElementById("table-allbookmark").innerHTML = bookmarksContainer;
+}
+
+function deleteBookmark(index) {
+    bookmarks.splice(index, 1);
+    localStorage.setItem('allBookmarks', JSON.stringify(bookmarks));
+    displayBookmarks(bookmarks); // إعادة عرض الـ bookmarks بعد الحذف
+}
+
+function clearBookmarkForm() {
+    titleInput.value = '';
+    urlInput.value = '';
+    titleInputUpdate.value = '';
+    urlInputUpdate.value = '';
+}
+
+function getUpdatAlert(index) {
+    bigWindowupdate.classList.remove('d-none');
+    titleInputUpdate.value = bookmarks[index].title;
+    urlInputUpdate.value = bookmarks[index].url;
+    lastIndex = index;
+}
+
+function updateBookmark() {
+    var updatedTitle = titleInputUpdate.value.trim();
+    var updatedURL = urlInputUpdate.value.trim();
+
+    if (updatedTitle === '' || updatedURL === '') {
+        closeUpdatAlert();
+        getAlert();
+        return;
+    }
+    if (updatedTitle.length < 3) {
+        closeUpdatAlert();
+        getAlert();
+        return;
+    }
+    if (!isValidURL(updatedURL)) {
+        closeUpdatAlert();
+        getAlert();
+        return;
+    }
+
+    for (var i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].title === updatedTitle && i !== lastIndex) {
+            closeUpdatAlert();
+            getAlert2();
+            return;
+        }
+        if (bookmarks[i].url === updatedURL && i !== lastIndex) {
+            closeUpdatAlert();
+            getAlert3();
+            return;
+        }
+    }
+    bookmarks[lastIndex].title = updatedTitle;
+    bookmarks[lastIndex].url = updatedURL;
+    localStorage.setItem('allBookmarks', JSON.stringify(bookmarks));
+    clearBookmarkForm();
+    closeUpdatAlert();
+    displayBookmarks(bookmarks); 
+}
+
+function getAlert() {
+    bigWindowAlert1.classList.remove('d-none');
+}
+
+function closeAlert() {
+    bigWindowAlert1.classList.add('d-none');
+}
+
+function getAlert2() {
+    bigWindowAlert2.classList.remove('d-none');
+}
+
+function closeAlert2() {
+    bigWindowAlert2.classList.add('d-none');
+}
+
+function getAlert3() {
+    bigWindowAlert3.classList.remove('d-none');
+}
+
+function closeAlert3() {
+    bigWindowAlert3.classList.add('d-none');
+}
+
+function closeUpdatAlert() {
+    bigWindowupdate.classList.add('d-none');
+}
+
+function searchBookmarks(searchkey) {
+    var result = [];
+    for (var i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].title.toLowerCase().trim().includes(searchkey.toLowerCase().trim())) {
+            result.push(bookmarks[i]);
+        }
+    }
+    displayBookmarks(result);
+}
